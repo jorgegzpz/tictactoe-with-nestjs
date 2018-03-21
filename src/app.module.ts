8e997fc0ee412b6bf54414200d5a8f9d79790cc5
Jorge González Pérez <jorgegonzalezperez@outlook.com>
2018-03-21 18:31:21 +0100

    Adds the main app module

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+
+import { Module } from "@nestjs/common";
+import { GamesModule } from "model/games/games.module";
+import { UsersModule } from "model/users/users.module";
+
+@Module({
+    imports: [
+        GamesModule,
+        UsersModule,
+    ],
+})
+export class ApplicationModule {}
