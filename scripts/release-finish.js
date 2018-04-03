d4ba5298b8859fc12a4a7e559e59797b096c47fc
Jorge González Pérez <jorgegonzalezperez@outlook.com>
2018-04-03 17:38:10 +0200

    Script updated

--- scripts/release-finish.js
+++ scripts/release-finish.js
@@ -10,36 +10,37 @@ const cb = (err, stdout, stderr) => {
 };
 
 const getBranch = (err, stdout, stderr) => {
+    branch = stdout;
     if (err) {
         return;
     }
-    branch = stdout;
+
+    if (branch !== branchName) {
+        console.error("Release can be finished only on release branch!")
+        return 1;
+    }
+
+    // Initialize gitflow
+    exec("git flow init -f -d", cb);
+
+    // Ensure you are on latest develop  & master and return back
+    exec("git checkout develop", cb);
+    exec("git pull origin develop", cb);
+    exec("git checkout -", cb);
+
+    exec("git checkout master", cb);
+    exec("git pull origin master", cb);
+    exec("git checkout -", cb);
+
+    exec("git checkout develop", cb);
+    exec("git merge " + branchName, cb);
+    exec("git checkout master", cb);
+    exec("git merge " + branchName, cb);
+    exec("git branch -d " + branchName, cb);
+    exec("git tag -a v" + version + " -m 'Release " + version + "'", cb);
+
+    exec("git push origin develop && git push origin master --tags", cb);
 };
 
 exec("git rev-parse --abbrev-ref HEAD", getBranch);
 
-if (branch !== branchName) {
-    console.error("Release can be finished only on release branch!")
-    return 1;
-}
-
-// Initialize gitflow
-exec("git flow init -f -d", cb);
-
-// Ensure you are on latest develop  & master and return back
-exec("git checkout develop", cb);
-exec("git pull origin develop", cb);
-exec("git checkout -", cb);
-
-exec("git checkout master", cb);
-exec("git pull origin master", cb);
-exec("git checkout -", cb);
-
-exec("git checkout develop", cb);
-exec("git merge " + branchName, cb);
-exec("git checkout master", cb);
-exec("git merge " + branchName, cb);
-exec("git branch -d " + branchName, cb);
-exec("git tag -a v" + version + " -m 'Release " + version + "'", cb);
-
-exec("git push origin develop && git push origin master --tags", cb);
